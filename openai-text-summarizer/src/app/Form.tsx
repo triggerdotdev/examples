2500227c4c567bfbb478bfd0b610984d66e62fd2
Matt Aitken <matt@mattaitken.com>
2023-07-27 11:43:46 +0100

    Use a very simple state machine for the form state

--- openai-text-summarizer/src/app/Form.tsx
+++ openai-text-summarizer/src/app/Form.tsx
@@ -9,16 +9,31 @@ import { CheckIcon } from "./checkIcon";
 const textLinkStyle =
   "hover:cursor-pointer underline underline-offset-2 decoration-slate-600 hover:decoration-slate-50 transition";
 
+type State =
+  | {
+      status: "idle";
+      text: string;
+    }
+  | {
+      status: "submitting";
+    }
+  | {
+      status: "processing";
+      eventId: string;
+    };
+
 export default function SendTextForm() {
-  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [formState, setFormState] = useState<State>({
+    status: "idle",
+    text: "",
+  });
 
   const handleSubmit = () => {
-    setHasSubmitted(true);
+    setFormState({ status: "submitting" });
   };
 
   const handleReset = () => {
-    setHasSubmitted(false);
-    setText("");
+    setFormState({ status: "idle", text: "" });
   };
 
   async function action(data: FormData) {
@@ -27,28 +42,18 @@ export default function SendTextForm() {
       window.alert("Please enter some text before clicking Summarize.");
       return;
     }
-    await sendText(text);
+    const event = await sendText(text);
+    setFormState({ status: "processing", eventId: event.id });
   }
-
-  const [text, setText] = useState("");
 
   const handleInventingOnPrincipleClick = (event: React.MouseEvent) => {
     event.preventDefault();
-    setText(howYCombinatorStarted);
+    setFormState({ status: "idle", text: howYCombinatorStarted });
   };
 
   return (
     <>
-      {hasSubmitted ? (
-        <div className="bg-slate-800 p-10 max-w-lg items-center rounded-md border border-slate-700 flex flex-col gap-10">
-          <CheckIcon />
-          <p className="text-center text-slate-400">
-            Your text has been submitted and a message in Slack should appear in
-            ~10 seconds.
-          </p>
-          <Button onClick={handleReset}>Try again</Button>
-        </div>
-      ) : (
+      {formState.status === "idle" ? (
         <>
           <p className="max-w-2xl text-center text-slate-400">
             Test it out by inserting the essay{" "}
@@ -68,14 +73,25 @@ export default function SendTextForm() {
             <textarea
               rows={16}
               name="text"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
+              value={formState.text}
+              onChange={(e) =>
+                setFormState({ status: "idle", text: e.target.value })
+              }
               placeholder="Paste some long text here or an article and click Summarize."
               className="text-white w-full bg-slate-800 rounded py-4 px-6 border border-slate-700"
             />
-            <Button disabled={text === ""}>✨ Summarize ✨</Button>
+            <Button disabled={formState.text === ""}>✨ Summarize ✨</Button>
           </form>
         </>
+      ) : (
+        <div className="bg-slate-800 p-10 max-w-lg items-center rounded-md border border-slate-700 flex flex-col gap-10">
+          <CheckIcon />
+          <p className="text-center text-slate-400">
+            Your text has been submitted and a message in Slack should appear in
+            ~10 seconds.
+          </p>
+          <Button onClick={handleReset}>Try again</Button>
+        </div>
       )}
     </>
   );
