fc232523d678c02c261fba49c52af7c1bf1bec36
Matt Aitken <matt@mattaitken.com>
2023-07-27 18:25:06 +0100

    Improved the layout when loading

--- openai-text-summarizer/src/app/SummarizeProgress.tsx
+++ openai-text-summarizer/src/app/SummarizeProgress.tsx
@@ -9,10 +9,6 @@ import { CheckCircleIcon } from "@heroicons/react/24/solid";
 export function SummarizeProgress({ eventId }: { eventId: string }) {
   const { isLoading, isError, data, error } = useEventRunDetails(eventId);
 
-  if (isLoading || !data) {
-    return <Loading />;
-  }
-
   if (isError) {
     return <p>Error</p>;
   }
@@ -20,34 +16,25 @@ export function SummarizeProgress({ eventId }: { eventId: string }) {
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-        {!data.tasks || data.tasks.length === 0 ? (
-          <Loading />
-        ) : (
-          data.tasks.map((task) => (
-            <div key={task.id} className="flex gap-2 items-center">
-              {task.status === "RUNNING" ? (
-                <Spinner className="w-6 h-6" />
-              ) : (
-                <CheckCircleIcon className="w-6 h-6 text-emerald-600" />
-              )}
-              <div className="flex gap-2 items-center">
-                {task.icon && (
-                  <CompanyIcon
-                    name={task.icon}
-                    className="w-4 h-4"
-                    variant="light"
-                  />
-                )}
-                <h4>{task.displayKey ?? task.name}</h4>
-              </div>
-            </div>
-          ))
-        )}
+        <>
+          <ProgressItem
+            completed={data?.tasks !== undefined && data.tasks.length > 0}
+            name="Starting up"
+          />
+          {data?.tasks?.map((task) => (
+            <ProgressItem
+              key={task.id}
+              completed={task.status === "COMPLETED"}
+              name={task.displayKey ?? task.name ?? ""}
+              icon={task.icon}
+            />
+          ))}
+        </>
       </div>
-      {data.output && (
-        <div className="flex flex-col gap-2">
+      {data?.output && (
+        <div className="flex flex-col gap-0.5">
           <h4 className="text-base font-semibold">Posted to Slack</h4>
-          <p className="text-slate-400 text-sm mb-2">{data.output.summary}</p>
+          <p className="text-slate-400 text-sm mb-4">{data.output.summary}</p>
           <ButtonLink href={"/"}>Summarize another</ButtonLink>
         </div>
       )}
@@ -55,10 +42,26 @@ export function SummarizeProgress({ eventId }: { eventId: string }) {
   );
 }
 
-function Loading() {
+type ProgressItemProps = {
+  icon?: string;
+  completed: boolean;
+  name: string;
+};
+
+function ProgressItem({ icon, completed, name }: ProgressItemProps) {
   return (
-    <div className="flex items-center justify-center">
-      <Spinner className="w-6 h-6" />
+    <div className="flex gap-2 items-center">
+      {completed ? (
+        <CheckCircleIcon className="w-6 h-6 text-emerald-600" />
+      ) : (
+        <Spinner className="w-6 h-6" />
+      )}
+      <div className="flex gap-1.5 items-center">
+        {icon && (
+          <CompanyIcon name={icon} className="w-5 h-5" variant="light" />
+        )}
+        <h4 className="text-base">{name}</h4>
+      </div>
     </div>
   );
 }
