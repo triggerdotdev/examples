4505eea1e15ee60406cfe644a57457fbc2e70a44
James Ritchie <james@jamesritchie.co.uk>
2024-08-21 15:51:08 +0100

    More helpful instruction for adding your project id

--- nextjs/server-actions/my-app/trigger.config.ts
+++ nextjs/server-actions/my-app/trigger.config.ts
@@ -1,7 +1,8 @@
 import type { TriggerConfig } from "@trigger.dev/sdk/v3";
 
 export const config: TriggerConfig = {
-  project: "proj_…",
+  // Replace <your-project-ref> with your project id: https://trigger.dev/docs/trigger-config
+  project: "<your-project-ref>",
   logLevel: "log",
   retries: {
     enabledInDev: true,
