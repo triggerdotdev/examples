f025da17f209a7a90d80ea6dbf35572fc42d346c
D-K-P <8297864+D-K-P@users.noreply.github.com>
2024-09-05 08:43:51 +0100

    Added basic and database webhook examples

---
+++ supabase/edge-functions/supabase/functions/database-webhook/index.ts
@@ -0,0 +1,31 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { tasks } from "npm:@trigger.dev/sdk@3.0.0-beta.55/v3";
+import type { helloWorldTask } from "../../../src/trigger/example.ts";
+
+console.log("Hello from 'database-webhook' function!");
+
+Deno.serve(async (req) => {
+  const payload = await req.json();
+
+  await tasks.trigger<typeof helloWorldTask>("hello-world", "hello");
+  console.log(payload ?? "No name provided");
+
+  return new Response("ok");
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/database-webhook' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
---
+++ supabase/edge-functions/supabase/functions/edge-function-trigger/index.ts
@@ -0,0 +1,25 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { tasks } from "npm:@trigger.dev/sdk@3.0.0-beta.55/v3";
+import type { helloWorldTask } from "../../../src/trigger/example.ts";
+
+Deno.serve(async (req) => {
+  await tasks.trigger<typeof helloWorldTask>("hello-world", "hello");
+  return new Response("OK");
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:,
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/hello-world' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
