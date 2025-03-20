c5d41dd51f80de056691f4b12a3ed8bea4197041
D-K-P <8297864+D-K-P@users.noreply.github.com>
2025-03-20 10:43:38 +0000

    split exports

--- monorepos/turborepo-prisma-tasks-package/packages/tasks/src/index.ts
+++ monorepos/turborepo-prisma-tasks-package/packages/tasks/src/index.ts
@@ -1,2 +1,1 @@
 export * from "@trigger.dev/sdk/v3"; // Export values and types from the Trigger.dev sdk
-export type { addNewUser } from "./trigger/addNewUser"; // Export generated types from trigger/addNewUser.ts
