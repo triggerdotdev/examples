2dff81cd8f2091b509fc4a7cd26470d5e58a4a25
D-K-P <8297864+D-K-P@users.noreply.github.com>
2025-09-25 18:03:38 +0100

    Added space

--- product-image-generator/trigger.config.ts
+++ product-image-generator/trigger.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   project: "proj_xjminyoyogxbrfipkrkt",
   runtime: "node",
   logLevel: "log",
+
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
   // See https://trigger.dev/docs/runs/max-duration
